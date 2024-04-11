a63fc478c8ecb571c6f058e0c758c04280f99343
afalfonsom <afalfonsom@gmail.com>
2024-04-11 16:41:09 -0500

    Archivo con explicaciones de los diferentes operadores en python

---
+++ 02_Operator.py
@@ -0,0 +1,30 @@
+#Operadores Arimeticos
+print(3+4)
+print(3-4)
+print(3*4)
+print(3/4)
+print(3 //4 )
+print(10 //3 )
+
+#Operadores comparativos
+print(3>4)
+print(3<4)
+print(3>=4)
+print(3<=4)
+print (3==4)
+print (3!=4)
+print (3>4 ==2)
+
+#orden alfabético por ASCII
+print("Hola "> "Python")
+print("Hola"<"Python")
+print("Hola">="Python")
+print("Hola"<="Python")
+print ("Hola"=="Python")
+print ("Hola"!="Python")
+
+#Operadores lógicos
+print("operadores Logicos")
+print(3>4 and "Hola "> "Python") 
+print(3>4 or "Hola "> "Python")
+print (not(3>4))
