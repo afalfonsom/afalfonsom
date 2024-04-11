1f981925263b0dfc7298889c4ae688a2f97e655e
afalfonsom <afalfonsom@gmail.com>
2024-04-10 19:44:48 -0500

    Archivo con tipos explicación de tipos de datos y como consultarlos desde Python

---
+++ Curso 2024 Python/# Hola Mundo.py
@@ -0,0 +1,20 @@
+# Esto es un comentario
+# Nuestro Hola mundo en Python
+print('Hola python')
+
+
+"""
+Este es un 
+comentario
+en varias 
+lineas
+"""
+
+"""
+Consultar el tipo de dato
+"""
+print(type("Hola soy un dato string"))
+print(type(5))
+print(type(5.1))
+print(type(True))
+print(type(1+1J))
