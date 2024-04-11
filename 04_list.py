8896302bb35318816d0b9d1744b2f1ae2cb84e01
afalfonsom <afalfonsom@gmail.com>
2024-04-11 17:44:22 -0500

    Funcionamiento de las listas

---
+++ 04_list.py
@@ -0,0 +1,12 @@
+#Lists
+
+my_list = list()
+my_other_list = []
+
+print(len(my_list))
+
+my_list = [35,24,62,52,30,30,17]
+print(my_list)
+print(len(my_list))
+
+my_other_list = [35,1.84,"Andres", "Alfonso"]
